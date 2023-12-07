08c615fdc3e00d0a6f2dfc539a2ae8a40a9525c5
Chate Billy Chilima <85109013+chate-hub@users.noreply.github.com>
2023-12-08 01:20:02 +0200

    Add files via upload
    This is my initial push request.  Files basically output text to the screen.

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello World I am getting started with Unity!");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
